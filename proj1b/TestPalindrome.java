19c67f3c29dfbb395e0770b2365f8937c1064060
EIMadrigal <renjingtaofc@163.com>
2020-06-08 22:15:33 +0800

    proj1b

--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestPalindrome {
-    /*// You must use this palindrome, and not instantiate
+    // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
 
@@ -14,5 +14,32 @@ public class TestPalindrome {
             actual += d.removeFirst();
         }
         assertEquals("persiflage", actual);
-    } Uncomment this class once you've created your Palindrome class. */
+    }
+
+    @Test
+    public void testIsPalindrome1() {
+        String[] words = {"", "a", "racecar", "aaaaab"};
+        boolean[] expected = {true, true, true, false};
+        boolean[] actual = new boolean[4];
+        for (int i = 0; i < words.length; ++i) {
+            actual[i] = palindrome.isPalindrome(words[i]);
+        }
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    /**
+     * since isPalindrome had been overloaded in Palindrome.java,
+     * so here we have 2 tests.
+     */
+    public void testIsPalindrome2() {
+        String[] words = {"flake", "a", "racecar", "aaabbb"};
+        boolean[] expected = {true, true, false, true};
+        boolean[] actual = new boolean[4];
+        CharacterComparator cc = new OffByOne();
+        for (int i = 0; i < words.length; ++i) {
+            actual[i] = palindrome.isPalindrome(words[i], cc);
+        }
+        assertArrayEquals(expected, actual);
+    }
 }
