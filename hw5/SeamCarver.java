5697370ccf1c16f86ba22f581d12d4d03e30cd21
EIMadrigal <renjingtaofc@163.com>
2020-06-20 19:20:08 +0800

    hw5

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -17,7 +17,7 @@ public class SeamCarver {
 
     // current picture
     public Picture picture() {
-        return picture;
+        return new Picture(picture);
     }
 
     public int width() {
